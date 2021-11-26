96abecb32e3e44c6921f62fd6dccd6d4aaeed7b2
Andreea Ciomag <andreea.mihaela789@gmail.com>
2021-11-26 12:05:48 +0200

    Add project files.

---
+++ Data/Ciomag_Andreea_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ciomag_Andreea_Lab8.Models;
+
+namespace Ciomag_Andreea_Lab8.Data
+{
+    public class Ciomag_Andreea_Lab8Context : DbContext
+    {
+        public Ciomag_Andreea_Lab8Context (DbContextOptions<Ciomag_Andreea_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ciomag_Andreea_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Ciomag_Andreea_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Migrations/20211124131529_PublishingDate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ciomag_Andreea_Lab8.Migrations
+{
+    public partial class PublishingDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Book",
+                type: "decimal(6,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublishingDate",
+                table: "Book",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublishingDate",
+                table: "Book");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(6,2)");
+        }
+    }
+}
